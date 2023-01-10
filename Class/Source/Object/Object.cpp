1f64066e856c85e246edbc2ac8be5b63d44aa8f7
大陸 永井 <k022g0053@m.neec.ac.jp>
2023-01-10 15:08:25 +0900

    Objectクラス 重力がかかるフラグを追加 フィールド外への移動を制御する関数を追加 当たり判定の処理の追加

--- Class/Source/Object/Object.cpp
+++ Class/Source/Object/Object.cpp
@@ -15,7 +15,7 @@ void Object::SuccessorInitialize(){
 	velocity = { 0,0 };
 	acceleration = { 0,0 };
 
-	isFlying = false;
+	isFlying = true;
 
 	texture = Novice::LoadTexture("white1x1.png");
 
@@ -23,16 +23,24 @@ void Object::SuccessorInitialize(){
 
 void Object::Update() {
 
-	// 加速度に重力を追加
-	acceleration.y += kGravitationalAcceleration;
-
 	// 加速度や速度を継承先で変更
 	SuccessorUpdate();
+
+	if (isFlying) {
+		// 加速度に重力を追加
+		acceleration.y -= kGravitationalAcceleration;
+	}
 
 	// 変更した加速度を追加
 	velocity.x += acceleration.x;
 	velocity.y += acceleration.y;
 
+	// 座標に速度分移動させる
+	centerPosition.x += velocity.x;
+	centerPosition.y += velocity.y;
+
+
+	CheckFieldHitBox();
 
 }
 
@@ -45,10 +53,44 @@ void Object::Draw() {
 }
 
 bool Object::CheckHitBox(Point hitPosition) {
+	// 左右
+	if (centerPosition.x - width / 2.0f < hitPosition.x && hitPosition.x < centerPosition.x + width / 2.0f) {
+		// 上下
+		if (centerPosition.y - height / 2.0f < hitPosition.y && hitPosition.y < centerPosition.y + height / 2.0f) {
+			return true;
+		}
+	}
 	return false;
 }
 
 bool Object::CheckHitBox(Box hitPosition) {
-	return false;
+	if (BaseMath::CheckHitBox({ BaseDraw::WorldtoScreen(centerPosition),width,height }, { BaseDraw::WorldtoScreen(hitPosition.position),hitPosition.width,hitPosition.height })){
+		return true;
+	}
+	else {
+		return false;
+	}
+}
 
+bool Object::CheckFieldHitBox() {
+	// フィールド外に出た時の処理
+	// 左右
+	if (centerPosition.x - width / 2.0f < 0) {
+		centerPosition.x -= centerPosition.x - width / 2.0f;
+		return true;
+	}
+	else if(1920 < centerPosition.x + width / 2.0f) {
+		centerPosition.x += 1920 - (centerPosition.x + width / 2.0f);
+		return true;
+	}
+	// 上下
+	if (centerPosition.y - height / 2.0f < 0) {
+		centerPosition.y -= centerPosition.y - height / 2.0f;
+		isFlying = false;
+		return true;
+	}
+	else if (1080 < centerPosition.y + height / 2.0f) {
+		centerPosition.y += 1080 - (centerPosition.y + height / 2.0f);
+		return true;
+	}
 }
