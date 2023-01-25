24a11a3b64c411de220ec281f0fa164c4e63df0f
祐介 戸田 <k022g0025@m.neec.ac.jp>
2023-01-25 22:46:33 +0900

    EnemyAttackHitBoxを実装 円の当たり判定のみ未実装

---
+++ Class/AttackHitBox/EnemyAttackHitBox.cpp
@@ -0,0 +1,99 @@
+#include "EnemyAttackHitBox.h"
+
+// 初期化
+void EnemyAttackHitBox::Initialize() {
+
+	// 現在の添え字
+	num = 0;
+
+	// 当たり判定のリスト
+	for (int i = 0; i < 128; i++) {
+		hitBox[i].centerPosition = { -15000,-15000 };
+		hitBox[i].width = 0;
+		hitBox[i].height = 0;
+		hitBox[i].angle = 0;
+		hitBox[i].atk = 0;
+	}
+
+	// 左の半円の当たり判定
+	leftEllipse.centerPosition = { -15000,-15000 };
+	leftEllipse.radius = 0;
+	leftEllipse.angle = 0;
+	leftEllipse.atk = 0;
+	// 右の半円の当たり判定
+	rightEllipse.centerPosition = { -15000,-15000 };
+	rightEllipse.radius = 0;
+	rightEllipse.angle = 0;
+	rightEllipse.atk = 0;
+}
+// 更新
+void EnemyAttackHitBox::Update() {
+	// なし
+}
+// 描画
+void EnemyAttackHitBox::Draw() {
+	// なし
+}
+
+
+
+// 新しく当たり判定を追加する関数（四角）
+// 引数：
+// centerPosition ... 真ん中の座標
+// width ... ヒットボックスの横幅（左上の点から）
+// height ... ヒットボックスの縦幅（左上の点から）
+// angle ... ヒット対象の四角の回転角度
+// atk ... ダメージ量
+void EnemyAttackHitBox::MakeNewHitBox(Point centerPosition, float width, float height, float angle, float atk) {
+	if (num >= 128) {
+		return;
+	}
+	
+	hitBox[num].centerPosition = centerPosition;
+	hitBox[num].width = width;
+	hitBox[num].height = height;
+	hitBox[num].angle = angle;
+	hitBox[num].atk = atk;
+
+	num++;
+}
+
+// 新しく当たり判定を追加する関数（左の円）
+// 引数：
+// centerPosition ... 真ん中の座標
+// width ... ヒットボックスの横幅（左上の点から）
+// height ... ヒットボックスの縦幅（左上の点から）
+// angle ... ヒット対象の四角の回転角度
+// atk ... ダメージ量
+void EnemyAttackHitBox::MakeNewHitBoxLeft(Point centerPosition, float radius, float angle, float atk) {
+	leftEllipse.centerPosition = centerPosition;
+	leftEllipse.radius = radius;
+	leftEllipse.angle = angle;
+	leftEllipse.atk = atk;
+}
+// 新しく当たり判定を追加する関数（右の円）
+// 引数：
+// centerPosition ... 真ん中の座標
+// width ... ヒットボックスの横幅（左上の点から）
+// height ... ヒットボックスの縦幅（左上の点から）
+// angle ... ヒット対象の四角の回転角度
+// atk ... ダメージ量
+void EnemyAttackHitBox::MakeNewHitBoxRight(Point centerPosition, float radius, float angle, float atk) {
+	rightEllipse.centerPosition = centerPosition;
+	rightEllipse.radius = radius;
+	rightEllipse.angle = angle;
+	rightEllipse.atk = atk;
+}
+
+
+// 当たり判定検証
+// 返り値：ヒットしていた場合 -> 被ダメージ量、　ヒットしていない場合 -> 0
+// 引数：
+// centerPosition ... 検証する座標
+float EnemyAttackHitBox::CheckHitBox(Point centerPosition) {
+	for (int i = 0; i < num; i++) {
+		if (BaseMath::CheckHitBox(hitBox[i].centerPosition, hitBox[i].width, hitBox[i].height, hitBox[i].angle, centerPosition)) {
+			return hitBox[i].atk;
+		}
+	}
+}
