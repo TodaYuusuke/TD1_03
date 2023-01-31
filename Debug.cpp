a1c628e5bdb6eef50d2c2cb436a2eade8617a673
夕輝 豊田 <K022G0030@g.neec.ac.jp>
2023-01-31 12:20:37 +0900

    操作方法を明示

--- Debug.cpp
+++ Debug.cpp
@@ -9,7 +9,6 @@ Debug::~Debug() {
 
 }
 
-
 // 初期化
 void Debug::Initialize() {
 	PublicFrag::Initialize();
@@ -19,6 +18,7 @@ void Debug::Initialize() {
 	wireManager.Initialize();
 	objectManager.MakeNewObjectPlayer({ 100,200 }, &wireManager);
 	boss.Initialize(&objectManager);
+
 }
 // 更新
 void Debug::Update() {
@@ -63,4 +63,20 @@ void Debug::Draw() {
 	Novice::DrawEllipse(BaseInput::GetMousePosition().x, BaseInput::GetMousePosition().y, 10, 10, 0, GREEN, kFillModeWireFrame);
 	Novice::DrawEllipse(BaseInput::GetMousePosition().x, BaseInput::GetMousePosition().y, 11, 11, 0, GREEN, kFillModeWireFrame);
 	Novice::DrawEllipse(BaseInput::GetMousePosition().x, BaseInput::GetMousePosition().y, 12, 12, 0, GREEN, kFillModeWireFrame);
+
+	// デバックの操作方法テクスチャ
+	int debugMoveTex = Novice::LoadTexture("./Resources/Texture/Debug/Move.png");
+	int debugShotTex = Novice::LoadTexture("./Resources/Texture/Debug/Shot.png");
+	int debugJumpTex = Novice::LoadTexture("./Resources/Texture/Debug/Jump.png");
+	int debugAttractTex = Novice::LoadTexture("./Resources/Texture/Debug/Attract.png");
+	int debugBGTex = Novice::LoadTexture("./Resources/Texture/Debug/BG.png");
+
+
+	Novice::DrawSprite(1280, 10, debugBGTex, 10, 0.5f, 0, WHITE);
+	// 動作方法
+	Novice::DrawSprite(1280, 10, debugMoveTex, 0.25f, 0.25f, 0.0f, WHITE);
+	Novice::DrawSprite(1600, 10, debugJumpTex, 0.25f, 0.25f, 0.0f, WHITE);
+	Novice::DrawSprite(1280, 42, debugShotTex, 0.25f, 0.25f, 0.0f, WHITE);
+	Novice::DrawSprite(1600, 42, debugAttractTex, 0.25f, 0.25f, 0.0f, WHITE);
+
 }
