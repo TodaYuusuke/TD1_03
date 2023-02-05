c7536463b47196dfe9e0e264d35aab338d0edb50
夕輝 豊田 <K022G0030@g.neec.ac.jp>
2023-02-06 04:04:25 +0900

    とりあえずの視差スクロールを実装 画像はこの後差し替える予定

--- Class/SceneManager/Scene/TutorialStage.cpp
+++ Class/SceneManager/Scene/TutorialStage.cpp
@@ -54,15 +54,28 @@ void TutorialStage::Draw() {
 	Novice::ScreenPrintf(0, 0, "Push ENTER to Skip Stage");
 
 	Point screenPosition = BaseDraw::GetScreenPosition();
+	Point playerPosition = objectManager.GetPlayerPosition();
+
+	Point screenSubtraction1 = { playerPosition.x / 8.0f ,0.0f };
+	Point screenSubtraction2 = { -(float)BaseConst::kWindowWidth / 2 - playerPosition.x / 2.0f ,0.0f };
 
 	// îwåi
-	BaseDraw::DrawQuad({screenPosition.x + BaseConst::kWindowWidth / 2, screenPosition.y - BaseConst::kWindowHeight / 2 },
-		BaseTexture::kBackGroundCity, {1920, 1080}, 1.1f, 0.0f, 0xFFFFFFFF);
 
-	BaseDraw::DrawQuad({screenPosition.x + BaseConst::kWindowWidth / 2, screenPosition.y - BaseConst::kWindowHeight / 2 },
-		BaseTexture::kBackGroundForest, {1920, 1080}, 1.1f, 0.0f, 0xFFFFFFFF);
+	for (int i = 0; i < 7; i++) {
+		BaseDraw::DrawQuad({ 
+			(float)BaseConst::kWindowWidth / 2 + ((float)BaseConst::kWindowWidth * 1.25f * i) + screenSubtraction1.x, 
+			screenPosition.y - (float)BaseConst::kWindowHeight / 2 },
+			BaseTexture::kBackGroundCity, { 1920, 1080 }, 1.25f, 0.0f, 0xFFFFFFFF);
+	}
 
-	BaseDraw::DrawSprite(screenPosition, BaseTexture::kBackGroundFrame, {1.0f, 1.0f}, 0.0f, 0xFFFFFFFF);
+	for (int i = 0; i < 10; i++) {
+		BaseDraw::DrawQuad({ (float)BaseConst::kWindowWidth / 2 + ((float)BaseConst::kWindowWidth * 1.25f * i) + screenSubtraction2.x,
+			screenPosition.y - (float)BaseConst::kWindowHeight / 2 },
+			BaseTexture::kBackGroundForest, { 1920, 1080 }, 1.25f, 0.0f, 0xFFFFFFFF);
+	}
+
+	BaseDraw::DrawQuad({ screenPosition.x + BaseConst::kWindowWidth / 2, screenPosition.y - BaseConst::kWindowHeight / 2 },
+		BaseTexture::kBackGroundFrame, { 1920, 1080 }, 1.35f, 0.0f, 0xFFFFFFFF);
 
 	MapManager::Draw();
 
@@ -70,4 +83,5 @@ void TutorialStage::Draw() {
 
 	objectManager.Draw();
 	wireManager.Draw();
+
 }
