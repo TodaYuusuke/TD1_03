0cd91980a67593a1279fe6a61418dd34642faa08
祐介 戸田 <k022g0025@m.neec.ac.jp>
2023-02-07 11:49:48 +0900

    チュートリアル更新

--- Class/SceneManager/Scene/TutorialStage.cpp
+++ Class/SceneManager/Scene/TutorialStage.cpp
@@ -97,6 +97,26 @@ void TutorialStage::Draw() {
 }
 
 
-void CheckPlayerProgress() {
+void TutorialStage::CheckPlayerProgress() {
+	
+	switch (playerProgress)
+	{
+		case 0:
+			break;
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		case 6:
+			break;
+		case 7:
+			break;
+	}
 
 }
